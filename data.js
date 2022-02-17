3e275d67b2cf4251460d72479df54bc47bd7487b
cxh <401158573@qq.com>
2022-02-17 22:49:56 +0800

    2022年2月17日大改，增加data.js以及temp.html提供便利生成页面

---
+++ data.js
@@ -0,0 +1,89 @@
+var data = {
+    'search': {
+        'title': '搜索',
+        'placeholder': '请输入搜索内容',
+        'button': '搜索',
+        'blank':'请输入要搜索的内容',
+        'radios': {
+            'bing': {
+                'name': '必应',
+                'logo': 'logo/bing.ico',
+                'url': 'https://cn.bing.com/search?q='
+            },
+            'baidu': {
+                'name': '百度',
+                'logo': 'logo/baidu.ico',
+                'url': 'https://www.baidu.com/s?wd='
+            },
+            'zhihu': {
+                'name': '知乎',
+                'logo': 'logo/zhihu.ico',
+                'url': 'https://www.zhihu.com/search?type=content&q='
+            },
+            'weibo': {
+                'name': '微博',
+                'logo': 'logo/weibo.ico',
+                'url': 'https://s.weibo.com/weibo/'
+            },
+        },
+    },
+    'code': {
+        'title': '搜代码',
+        'placeholder': '请输入搜索内容',
+        'button': '搜索',
+        'blank':'请输入要搜索的内容',
+        'radios': {
+            'csdn': {
+                'name': 'csdn',
+                'logo': 'logo/csdn.ico',
+                'url': 'https://so.csdn.net/so/search?q='
+            },
+            'blog': {
+                'name': '博客园',
+                'logo': 'logo/博客园.ico',
+                'url': 'https://zzk.cnblogs.com/s?w='
+            },
+            'github': {
+                'name': 'github',
+                'logo': 'logo/github.ico',
+                'url': 'https://github.com/search?q='
+            },
+        },
+    },
+    'video': {
+        'title': '搜视频',
+        'placeholder': '请输入搜索内容',
+        'button': '搜索',
+        'blank':'请输入要搜索的内容',
+        'radios': {
+            'tencent': {
+                'name': '腾讯视频',
+                'logo': 'logo/腾讯视频.ico',
+                'url': 'https://v.qq.com/x/search/?q='
+            },
+            'iqiyi': {
+                'name': '爱奇艺',
+                'logo': 'logo/爱奇艺.png',
+                'url': 'https://so.iqiyi.com/so/q_'
+            },
+            'bilibili': {
+                'name': 'B站',
+                'logo': 'logo/哔哩哔哩.ico',
+                'url': 'https://search.bilibili.com/all?keyword='
+            },
+        },
+    },
+    'jeixi': {
+        'title': '视频解析',
+        'placeholder': '请输入视频链接',
+        'button': '解析',
+        'blank':'请输入要解析的视频链接',
+        'radios': {
+            'jiexi1': {
+                'name': '解析1',
+                'logo': '',
+                'url': 'https://jx.parwix.com:4433/player/analysis.php?v='
+            },
+        },
+    },
+}
