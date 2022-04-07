637f0ab5eed8d76bc7916bf678f9696393453a6b
cxh <401158573@qq.com>
2022-04-07 23:34:41 +0800

    不用组件时，这是可用的

--- js/data.js
+++ js/data.js
@@ -1,127 +1,139 @@
-var data = {
-    'search': {
-        'title': '搜索',
-        'placeholder': '请输入搜索内容',
-        'button': '搜索',
-        'blank':'请输入要搜索的内容',
-        'radios': {
-            'bing': {
-                'name': '必应',
-                'logo': 'logo/bing.ico',
-                'url': 'https://cn.bing.com/search?q={0}'
-            },
-            'baidu': {
-                'name': '百度',
-                'logo': 'logo/baidu.ico',
-                'url': 'https://www.baidu.com/s?wd={0}'
-            },
-            'zhihu': {
-                'name': '知乎',
-                'logo': 'logo/zhihu.ico',
-                'url': 'https://www.zhihu.com/search?type=content&q={0}'
-            },
-            'weibo': {
-                'name': '微博',
-                'logo': 'logo/weibo.ico',
-                'url': 'https://s.weibo.com/weibo/{0}'
-            },
-        },
-    },
-    'code': {
-        'title': '搜代码',
-        'placeholder': '请输入搜索内容',
-        'button': '搜索',
-        'blank':'请输入要搜索的内容',
-        'radios': {
-            'csdn': {
-                'name': 'csdn',
-                'logo': 'logo/csdn.ico',
-                'url': 'https://so.csdn.net/so/search?q={0}'
-            },
-            'blog': {
-                'name': '博客园',
-                'logo': 'logo/博客园.ico',
-                'url': 'https://zzk.cnblogs.com/s?w={0}'
-            },
-            'github': {
-                'name': 'github',
-                'logo': 'logo/github.ico',
-                'url': 'https://github.com/search?q={0}'
-            },
-        },
-    },
-    'video': {
-        'title': '搜视频',
-        'placeholder': '请输入搜索内容',
-        'button': '搜索',
-        'blank':'请输入要搜索的内容',
-        'radios': {
-            'tencent': {
-                'name': '腾讯视频',
-                'logo': 'logo/腾讯视频.ico',
-                'url': 'https://v.qq.com/x/search/?q={0}'
-            },
-            'iqiyi': {
-                'name': '爱奇艺',
-                'logo': 'logo/爱奇艺.png',
-                'url': 'https://so.iqiyi.com/so/q_{0}'
-            },
-            'bilibili': {
-                'name': 'B站',
-                'logo': 'logo/哔哩哔哩.ico',
-                'url': 'https://search.bilibili.com/all?keyword={0}'
-            },
-        },
-    },
-    'jiexi': {
-        'title': '视频解析',
-        'placeholder': '请输入视频链接',
-        'button': '解析',
-        'blank':'请输入要解析的视频链接',
-        'radios': {
-            'jiexi1': {
-                'name': '解析1',
-                'logo': '',
-                'url': 'https://jx.parwix.com:4433/player/analysis.php?v={0}'
-            },
-        },
-    },
-    'download': {
-        'title': '下载解析',
-        'placeholder': '请输入下载链接',
-        'button': '下载',
-        'blank':'请输入要下载的文件链接',
-        'radios': {
-            'githubjiexi': {
-                'name': 'github',
-                'logo': 'logo/github.ico',
-                'url': 'https://ghproxy.com/?q={0}'
-            },
-        },
-    },
-    'qrcode': {
-        'title': '二维码生成',
-        'placeholder': '请输入二维码内容',
-        'button': '生成',
-        'blank':'请输入二维码内容',
-        'logo':'logo/qrcode.jpg',
-        'extend':{
-            'html':'<div id="qrcodearea" style="height:0;"><img src="" class="qrcode-img" style="width: 50%;border-radius: calc(var(--size)*6/100);"></div>',
-            'js':'document.querySelector("#qrcodearea").style.height="auto";\
-                document.querySelector(".qrcode-img").src="https://api.pwmqr.com/qrcode/create/?url="+content.value;'
+var data = [
+    {
+        name: 'search',
+        title: '搜索',
+        input_box: {
+            placeholder: '请输入搜索内容',
+            button: '搜索',
+            if_blank: '请输入要搜索的内容',
+            radios: [
+                {
+                    name: '必应',
+                    logo: 'logo/bing.ico',
+                    url: 'https://cn.bing.com/search?q={0}'
+                },
+                {
+                    name: '百度',
+                    logo: 'logo/baidu.ico',
+                    url: 'https://www.baidu.com/s?wd={0}'
+                },
+                {
+                    name: '知乎',
+                    logo: 'logo/zhihu.ico',
+                    url: 'https://www.zhihu.com/search?type=content&q={0}'
+                },
+                {
+                    name: '微博',
+                    logo: 'logo/weibo.ico',
+                    url: 'https://s.weibo.com/weibo/{0}'
+                },
+            ],
+            execute: 'open_new_page',
         }
     },
-    'other': {
-        'title': '网页跳转',
-        'placeholder': '百度',
-        'button': '跳转',
-        'blank':'请输入网址',
-        'radios': {
-            'baidu1': {
-                'name': '百度',
-                'logo': 'logo/baidu.ico',
-                'url': 'https://www.musiccopilot.club/baidu/?{0}'
-            },
-        },
+    {
+        name: 'code',
+        title: '搜代码',
+        input_box: {
+            placeholder: '请输入搜索内容',
+            button: '搜索',
+            if_blank: '请输入要搜索的内容',
+            radios: [
+                {
+                    name: 'csdn',
+                    logo: 'logo/csdn.ico',
+                    url: 'https://so.csdn.net/so/search?q={0}'
+                },
+                {
+                    name: '博客园',
+                    logo: 'logo/博客园.ico',
+                    url: 'https://zzk.cnblogs.com/s?w={0}'
+                },
+                {
+                    name: 'github',
+                    logo: 'logo/github.ico',
+                    url: 'https://github.com/search?q={0}'
+                }
+            ],
+            execute: 'open_new_page',
+        }
     },
-}
+    {
+        name: 'video',
+        title: '搜视频',
+        input_box: {
+            placeholder: '请输入搜索内容',
+            button: '搜索',
+            if_blank: '请输入要搜索的内容',
+            radios: [
+                {
+                    name: '腾讯视频',
+                    logo: 'logo/腾讯视频.ico',
+                    url: 'https://v.qq.com/x/search/?q={0}'
+                },
+                {
+                    name: '爱奇艺',
+                    logo: 'logo/爱奇艺.png',
+                    url: 'https://so.iqiyi.com/so/q_{0}'
+                },
+                {
+                    name: 'B站',
+                    logo: 'logo/哔哩哔哩.ico',
+                    url: 'https://search.bilibili.com/all?keyword={0}'
+                }
+            ],
+            execute: 'open_new_page',
+        }
+    },
+    {
+        name: 'jiexi',
+        title: '视频解析',
+        input_box: {
+            placeholder: '请输入视频链接',
+            button: '解析',
+            if_blank: '请输入要解析的视频链接',
+            radios: [
+                {
+                    name: '解析1',
+                    logo: '',
+                    url: 'https://jx.parwix.com:4433/player/analysis.php?v={0}'
+                },
+            ],
+            execute: 'open_new_page',
+        }
+    },
+    {
+        name: 'download',
+        title: '下载解析',
+        input_box: {
+            placeholder: '请输入下载链接',
+            button: '下载',
+            if_blank: '请输入要下载的文件链接',
+            radios: [
+                {
+                    name: 'github',
+                    logo: 'logo/github.ico',
+                    url: 'https://ghproxy.com/?q={0}'
+                },
+            ],
+            execute: 'open_new_page',
+        }
+    },
+    {
+        name: 'qrcode',
+        title: '二维码生成',
+        input_box: {
+            placeholder: '请输入二维码内容',
+            button: '生成',
+            if_blank: '请输入二维码内容',
+            logo: 'logo/qrcode.jpg',
+            extend: {
+                'html': '<div id="qrcodearea" style="height:0;"><img src="" class="qrcode-img" style="width: 50%;border-radius: calc(var(--size)*6/100);"></div>',
+                'js': 'document.querySelector("#qrcodearea").style.height="auto";\
+                document.querySelector(".qrcode-img").src="https://api.pwmqr.com/qrcode/create/?url="+content.value;'
+            },
+            execute: 'eval_my_js',
+        }
+    },
+];
+
